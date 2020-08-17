c9cae6c0861e8a869d81a3f4bbd43bae4aa6a9a7
Steven Liu <52724724+miduoliu@users.noreply.github.com>
2020-08-16 19:32:29 -0700

    Update Maximum Subarray.cpp

--- Maximum Subarray.cpp
+++ Maximum Subarray.cpp
@@ -16,10 +16,10 @@ public:
         int fin = 0;
         for (auto n : nums) {
             fin = fin + n;
-            if (cur < fin)
-                cur = fin;
-            if (fin < 0)
-                fin = 0;
+            if (cur < fin) //Through this process, let cur become the first num in numms;
+                cur = fin; //Check if the sum of cur and next num in nums is larger than cur;  
+            if (fin < 0) 
+                fin = 0; // Add a negative number is less that a num itself, so convert fin to 0, jump over the cur number in nums.
         }
         return cur;
     }
